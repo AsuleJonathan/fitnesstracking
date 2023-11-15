47aeb37c8b66255e644693e4021a2b7f3d5fa65d
AsuleJonathan <asulejona@gmail.com>
2023-11-15 08:11:15 +0300

    Implementation of bean utils
    Implementation of bean utils to serialize form parameters to an object

--- src/main/java/com/asule/app/action/MembersAction.java
+++ src/main/java/com/asule/app/action/MembersAction.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.math.BigDecimal;
+import org.apache.commons.beanutils.BeanUtils;
+import java.lang.reflect.InvocationTargetException;
 
 @WebServlet("/members")
 public class MembersAction extends HttpServlet {
